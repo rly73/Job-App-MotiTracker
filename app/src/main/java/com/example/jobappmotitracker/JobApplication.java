acd4ed7fe536daf95838872407a4f236c20f8130
thelostasian <rly@g.clemson.edu>
2020-02-09 00:24:24 -0800

    Create jobApplication object

--- app/src/main/java/com/example/jobappmotitracker/JobApplication.java
+++ app/src/main/java/com/example/jobappmotitracker/JobApplication.java
@@ -1,4 +1,51 @@
 package com.example.jobappmotitracker;
 
+import java.util.Date;
+
 public class JobApplication {
+    String companyName;
+    String jobPosition;
+    String notes;
+    Date dateApplied;
+    double pay;
+
+    public String companyName(){
+        return this.companyName;
+    }
+
+    public void setCompanyName(String name){
+        this.companyName = name;
+    }
+
+    public String getJobPosition(){
+        return this.jobPosition;
+    }
+
+    public void setJobPosition(String position){
+        this.jobPosition = position;
+    }
+
+    public Date getDateApplied(){
+        return this.dateApplied;
+    }
+
+    public void setDateApplied(Date appliedDate){
+        this.dateApplied = appliedDate;
+    }
+
+    public String notes(){
+        return this.notes;
+    }
+
+    public void setNotes(String text){
+        this.notes = text;
+    }
+
+    public double getPay(){
+        return this.pay;
+    }
+
+    public void setPay(double payGrade){
+        this.pay = payGrade;
+    }
 }
