8ea2c1798d3e88a130f8a460252be643b63990ba
thelostasian <rly@g.clemson.edu>
2020-02-15 13:23:05 -0800

    Created Database Helper

--- app/src/main/java/com/example/jobappmotitracker/MainActivity.java
+++ app/src/main/java/com/example/jobappmotitracker/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.jobappmotitracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
